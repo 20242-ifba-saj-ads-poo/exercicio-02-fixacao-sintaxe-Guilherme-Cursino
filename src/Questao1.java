4fdfbe333065c18df1776e1e92fd839c583e1ff5
Guilherme Henrique <eu.guilherme.henriq@gmail.com>
2024-11-09 20:22:32 -0300

    Questões 1, 2 e 3 respondidas

--- src/Questao1.java
+++ src/Questao1.java
@@ -1,5 +1,11 @@
 public class Questao1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Imprima todos os números de 150 a 300.
+        int i;
+
+        for(i = 150; i <= 300; i++){
+            System.out.println(i);
+        }
+
     }
 }
